db082876ba2eabc98b7cede1739836f962438c24
andrealtobelli <andrerogerio03@gmail.com>
2024-04-29 11:56:49 -0300

    hosting

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 // console.log(document.getElementById("botao").innerText);
 
 // Recuperando o botão do html e colocando em uma constante.
-const btnElemento = document.getElementById("botao");
+//const btnElemento = document.getElementById("botao");
 // Atrelar ao botão um evento de click.
-btnElemento.addEventListener("click",function() {
-    console.log("Botão clicado!");
-});
+//btnElemento.addEventListener("click",function() {
+//    console.log("Botão clicado!");
+//});
+//
+//hosting
+// var nome = "Cleiton";
+//  console.log(nome);
+
+//  if(true){
+//     var nome = "José";
+//  }
+
+//  console.log(nome);
