57436885abb13c28ad9a577bc34b5dffb4f3f4ae
victoragrela <victoragrela2@gmail.com>
2024-04-29 11:45:44 -0300

    iniciando com o JavaScript básico

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//console.log('Hello World!')
+
+//console.log(document.getElementById(botao).innerText);
+
+const btnElemento = documento.getElementId("botão");
+
+btnElemento.addEventListener("click", function () {
+    console.log("Botão clicado!");
+});
