4834834777c858f7600d4184682874f6358cebd0
dudatiemiak <rm554756@fiap.com.br>
2024-04-29 11:56:28 -0300

    Hoisting

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@
 // console.log(document.getElementById("botao").innerText);
 
 //Recuperando o botão do HTML e colocando em uma constante
-const btnElemento = document.getElementById("botão");
+// const btnElemento = document.getElementById("botão");
 //Atrelar ao botão um evento de click
-btnElemento.addEventListener("click",function () {
-    console.log("Botão clicado!");
-});
+// btnElemento.addEventListener("click",function () {
+    // console.log("Botão clicado!");});
+
+//Hoisting(elevação)
+// var nome = "Cleiton"; //Quando declaramos com var levamos o objeto declarado para o topo
+//  console.log(nome);
+ 
+//  if(true){
+//     let nome = "José";
+// }
+
+//  console.log(nome);
