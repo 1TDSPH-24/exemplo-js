e920709678917227f82dd155f3111a2445b0db46
dudatiemiak <rm554756@fiap.com.br>
2024-04-29 11:46:31 -0300

    Iniciando conceitos básicos.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+// console.log("Olá Mundo!");
+
+// console.log(document.getElementById("botao").innerText);
+
+//Recuperando o botão do HTML e colocando em uma constante
+const btnElemento = document.getElementById("botão");
+//Atrelar ao botão um evento de click
+btnElemento.addEventListener("click",function () {
+    console.log("Botão clicado!");
+});
