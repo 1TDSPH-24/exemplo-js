8cce506d1b009801ed9c260c7bb814b456447297
lecristina <cristinale33@hotmail.com>
2024-04-29 11:43:03 -0300

    Adicionei ação de botão clicando

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// console.log("Olá Mundo!"); --para comentar CTRL + ;
+
+// console.log(document.getElementById("botao").innerText);
+
+//Recuperando o botão do HTML e colocando em uma constante.
+const btnElemento = document.getElementById("botao")
+//Atrelar ao botão um evento de click.
+btnElemento.addEventListener( "click", function() {
+    console.log("Botão clicando");
+});
+//CRTL + ESPAÇO --> mostra as opções se deixar selecionada algum elemento
