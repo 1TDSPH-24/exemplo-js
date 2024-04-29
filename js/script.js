0b1df40c19d1f8df87b91ba05a1ab6e1cf4004ec
victoragrela <victoragrela2@gmail.com>
2024-04-29 11:56:46 -0300

    hoisting

--- js/script.js
+++ js/script.js
@@ -2,8 +2,19 @@
 
 //console.log(document.getElementById(botao).innerText);
 
-const btnElemento = documento.getElementId("botão");
+// const btnElemento = documento.getElementId("botão");
 
-btnElemento.addEventListener("click", function () {
-    console.log("Botão clicado!");
-});
+// btnElemento.addEventListener("click", function () {
+//     console.log("Botão clicado!");
+// });
+
+//Hoisting
+
+// var nome = "Cleiton";
+// console.log(nome);
+
+// if(true){
+//     let nome = "José";
+// }
+
+// console.log(nome);
