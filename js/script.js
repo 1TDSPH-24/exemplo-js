76a1ef1f9725305fca332df48038a002f4f1bbd6
sofiapetruk <sofiapetruk900@gmail.com>
2024-04-29 11:59:09 -0300

    como funciona js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// console.log("Olá Mundo")
+
+// console.log(document.getElementById("botao").innerText);
+
+// Recuperando o botão do HTML e colocando em um constante.
+//const btnElemento = document.getElementById("botao");
+// Atrelar ao botão um evento de click.
+//btnElemento.addEventListener("click", function () {
+//    console.log("Botão clicado!");
+//}) // um parâmetro de string 
+
+//hoistring --> significa elevação || tentar usar o menos o possível o 'VAR' (ela é antiga) pq qualquer pessoa pode tentar pegar no login 
+var nome ="Cleiton";
+console.log(nome);
+
+if(true){
+    let nome = "José";
+}
