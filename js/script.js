d128fd4ef10475f2c983efeff684f80efe6729f5
giovannisouzal <giovannisouzal2005@gmail.com>
2024-04-29 11:49:34 -0300

    pequena alteração no javascript

--- js/script.js
+++ js/script.js
@@ -4,8 +4,20 @@
 // console.log(document.getElementById("botao").innerHTML);
 
 //Recuperando o botão do HTML e colocando em uma constante.
-const btnElemento = document.getElementById("botao");
-//Atrelar ao botão um evento de click.
-btnElemento.addEventListener("click",function () {
-    console.log("Botão clicado!");
-});
+// const btnElemento = document.getElementById("botao");
+// //Atrelar ao botão um evento de click.
+// btnElemento.addEventListener("click",function () {
+//     console.log("Botão clicado!");
+// });
+
+//Hoisting
+var nome = "Cleiton";
+ console.log(nome);
+ 
+ if(true){
+    var nome = "José";
+ }
+
+ console.log(nome);
+
+ 
