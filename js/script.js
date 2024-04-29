27696174c5be47fa3d43f7376f785586f1f2e13b
andre.abreu20 <andre.luismesquita@hotmail.com>
2024-04-29 11:56:01 -0300

    Hoisting

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-const botao = document.getElementById("botao");
+// console.log("Olá Mundo!");
 
-botao.addEventListener("click", function () {
-    console.log("Botão clicado")
-})
+// console.log(document.getElementById("botao").innerHTML);
+
+//Recuperando o botão do HTML e colocando em uma constante.
+// const btnElemento = document.getElementById("botao");
+// //Atrelar ao botão um evento de click.
+// btnElemento.addEventListener("click",function () {
+//     console.log("Botão clicado!");
+// });
+
+//Hoisting
+/* var nome = "Cleiton";
+ console.log(nome);
+ 
+ if(true){
+    var nome = "José";
+ }
+
+ console.log(nome);
+ */
+ 
