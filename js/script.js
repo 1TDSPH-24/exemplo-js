71c20f973b6cb1f920d4962c87c9104674f6537f
vitoraraujo-144 <vitorvvaa@hotmail.com>
2024-04-29 11:56:40 -0300

    Hoisting

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// console.log("Olá Mundo!");
+
+// console.log(document.getElementById("botao").innerText);
+
+// Recuperando o botão do HTML e colocando em uma constante.
+// const btnElemento = document.getElementById("botao");
+// // Atrelar ao botão um elemento de click
+// btnElemento.addEventListener("click",function () {
+//     console.log("Botão clicado!");
+// });
+
+//Hoisting
+// let nome = "Sigward";
+// console.log(nome);
+
+// if(true){
+//     let nome = "Nosferatu";
+// }
+
+// console.log(nome);
