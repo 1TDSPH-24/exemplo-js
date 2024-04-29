8150e3fa3fa9eb7907be05cc847715c2c19a7141
ISRAEL2491 <israelvieira764@gmail.com>
2024-04-29 11:56:05 -0300

    Hoisting

--- js/script.js
+++ js/script.js
@@ -6,6 +6,18 @@
 //Recuperando o botao do html e colocando em uma constante.
 const btnElemento = document.getElementById("botao");
 //atrelar ao botao um evento de click.
-btnElemento.addEventListener("click",function() {
-    console.log("Botão clicado!");
-});
+// btnElemento.addEventListener("click",function() {
+//     console.log("Botão clicado!");
+// });
+
+//Hoisting
+var nome = "Cleiton";
+    console.log(nome);
+
+if(true){
+    let nome = "José";
+
+}
+
+console.log(nome);
+
