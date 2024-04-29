99c6f0a68b17368295a8d16e669d87233d8dae94
ovitortadeu <souavitor6@gmail.com>
2024-04-29 11:43:30 -0300

    iniciando javascript e etc e tals

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// console.log("Olá Mundo!");
+
+// console.log(document.getElementById("botao").innerText);
+
+const btnElemento = document.getElementById("botao");
+
+btnElemento.addEventListener("click",function(){
+    console.log("botao clickado top 10")
+});
