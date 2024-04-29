373f600ab4762dbcd81398769a3bbb2f58ca18c7
gusandrini <gustavosandrini92@gmail.com>
2024-04-29 11:57:37 -0300

    hoisting

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
  
- // console.log(Olá Mundo!);
+    // console.log(Olá Mundo!);
 
- //console.log(document.getElementById("botao").innerText);
+      //console.log(document.getElementById("botao").innerText);
  
- // recuperando o botao do html e colocando em uma constante
- const bntElemento = document.getElementById("botao")
- // atrelar ao botao um evento de click.
- bntElemento.addEventListener("click", function() {
-    console.log("Botão clicado!");
- });
+    // recuperando o botao do html e colocando em uma constante
+ //const bntElemento = document.getElementById("botao")
+    // atrelar ao botao um evento de click.
+ //bntElemento.addEventListener("click", function() {
+    //console.log("Botão clicado!");
+ //});
+
+ //Hoisting
+//var nome = "Cleiton";
+//console.log(nome);
+
+//if(true){
+//   let nome = "José";
+//}
+
+//console.log(nome);
