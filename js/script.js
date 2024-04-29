0f73a43667232e9dad04c22dc732358566401f29
lecristina <cristinale33@hotmail.com>
2024-04-29 11:56:24 -0300

    sobre hoisting

--- js/script.js
+++ js/script.js
@@ -3,9 +3,22 @@
 // console.log(document.getElementById("botao").innerText);
 
 //Recuperando o botão do HTML e colocando em uma constante.
-const btnElemento = document.getElementById("botao")
+//const btnElemento = document.getElementById("botao")
 //Atrelar ao botão um evento de click.
-btnElemento.addEventListener( "click", function() {
-    console.log("Botão clicando");
-});
+//btnElemento.addEventListener( "click", function() {
+//    console.log("Botão clicando");
+//});
 //CRTL + ESPAÇO --> mostra as opções se deixar selecionada algum elemento
+
+
+//Hoisting
+
+// var nome = "Cleiton";
+//     console.log(nome);
+
+//     if(true){
+//         let nome = "José";
+//     }
+
+//     console.log(nome);
+
