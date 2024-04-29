cc0a2213ec6496160aa80722a94872673a2ff27f
Victor-Estrella <victorhcarracci@gmail.com>
2024-04-29 11:56:32 -0300

    Hoisting

--- js/script.js
+++ js/script.js
@@ -4,8 +4,18 @@
 //console.log(document.getElementById("botao").innerText);
 
 //Recuperando o botão do HTML e colocando em uma constante.
-const btnElemento = document.getElementById("botao");
+//const btnElemento = document.getElementById("botao");
 //Atrelar ao botão um evento de click.
-btnElemento.addEventListener("click",function() {
-    console.log("Botão clicado!");
-});
+//btnElemento.addEventListener("click",function() {
+//    console.log("Botão clicado!");
+//});
+
+//Hoisting -significa elevação
+var nome = "Cleiton";
+console.log(nome);
+
+if(true){
+    let nome = "José";
+}
+
+console.log(nome);
