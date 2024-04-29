2e46727f09565700b18a685fae0c19e0f79f2588
cotete <feliperibeirots@gmail.com>
2024-04-29 11:54:17 -0300

    exercicio feito em aula

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//^w^        :3           ><           >.<          >w<           OwO           UwU
+
+// console.log(document.getElementById("butao"))
+
+const btnElemento = document.getElementById("butao");
+
+btnElemento.addEventListener("click", function(){
+    console.log("butao clicado");
+})
