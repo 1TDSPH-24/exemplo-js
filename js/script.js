706b391cd0063347041c37f9ff04159459ace556
gusandrini <gustavosandrini92@gmail.com>
2024-04-29 11:42:25 -0300

    iniciando conceitos básicos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+ 
+ // console.log(Olá Mundo!);
+
+ //console.log(document.getElementById("botao").innerText);
+ 
+ // recuperando o botao do html e colocando em uma constante
+ const bntElemento = document.getElementById("botao")
+ // atrelar ao botao um evento de click.
+ bntElemento.addEventListener("click", function() {
+    console.log("Botão clicado!");
+ });
