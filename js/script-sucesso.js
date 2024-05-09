23f937c8e90ad2ef229f426b1a14d6f9e1840874
enricodelguerra <ricodelguerra@gmail.com>
2024-05-09 09:46:35 -0300

    a

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -3,7 +3,17 @@
 //Recuperando o objeto atraves do localStorage
 let usuario = JSON.parse(localStorage.getItem("usuario-validado"));
 
+if(usuario){
 console.log(usuario.nomeCompleto);
 
 const msgWelcome = document.querySelector("#bem-vindo");
 msgWelcome.innerText = usuario.nomeCompleto;
+
+const logoutUser = document.querySelector("logout-user");
+logoutUser.addEventListener("click",()=>{
+    localStorage.removeItem("usuario-logado");
+    window.location.href = "../index.html"
+});
+}else{
+    window.location.href = "../index.html";
+}
