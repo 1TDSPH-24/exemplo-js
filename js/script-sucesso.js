9b3fdbc3ec95ade029b63b9f658e8bb0c7654da1
ISRAEL2491 <israelvieira764@gmail.com>
2024-05-09 09:37:11 -0300

    Fazendo o JS da pagina sucesso

--- js/script-sucesso.js
+++ js/script-sucesso.js
@@ -1,8 +1,19 @@
 
-//Recuperando o objeto enviado atraves do LocalStorage
-let usuario = JSON.parse(localStorage.getItem("usuario-valido"))
 
+//Recuperando o objeto enviado atrav'es do localStorage.
+let usuario = JSON.parse(localStorage.getItem("usuario-logado"));
+
+if(usuario){
 console.log(usuario.nomeCompleto);
 
 const msgWelcome = document.querySelector("#bem-vindo");
-msgWelcome.iinerText = usuario.nomeCompleto;
+msgWelcome.innerText = usuario.nomeCompleto;
+
+const logoutUser = document.querySelector("#logout-user");
+logoutUser.addEventListener("click",()=>{
+    localStorage.removeItem("usuario-logado");
+    window.location.href = "../index.html";
+});
+}else{
+    window.location.href = "../index.html";
+}
