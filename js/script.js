29e142f31f69b022831aac2af185e26a96972e0c
andrealtobelli <andrerogerio03@gmail.com>
2024-05-09 09:03:46 -0300

    Redirect com classes de status.

--- js/script.js
+++ js/script.js
@@ -186,22 +186,30 @@ let listaUsuario = [
   
   function validar(inputEmail, inputSenha) {
   
-    listaUsuario.forEach(function(usuario) {
-    
-      if ((inputEmail.value == usuario.emailUsuario) && (inputSenha.value == usuario.senhaUsuario)) {
-        //Redirect
-        console.log("Login realizado com sucesso!");
-        window.location.href = "../sucesso.html";
-        return false;
-      }
-      console.log(inputEmail.value)
-      console.log(usuario.emailUsuario)
-      console.log(inputSenha.value)
-      console.log(usuario.senhaUsuario)
-  });
+  //Recuperar elemento de msgStatus
+  let msgStatus = document.querySelector(".valida");
   
-      console.log("Login ou senha invalidos!");
-      window.location.href = "../erro.html";
+  for (let x = 0; x < listaUsuario.length; x++) {
+      
+      if ((inputEmail.value == listaUsuario[x].emailUsuario) && (inputSenha.value == listaUsuario[x].senhaUsuario)) {
+        //Redirect
+          msgStatus.setAttribute("class","sucesso");
+          msgStatus.innerText = "Login realizado com sucesso!";
+          setTimeout(function(){
+              msgStatus.setAttribute("class","valida");
+              msgStatus.innerText = "";
+              window.location.href = "../sucesso.html";
+          }, 3000);
+          return false;
+      }
+  }
+      msgStatus.setAttribute("class","erro");
+      msgStatus.innerText = "Login ou senha invalidos!";
+      setTimeout(function(){
+          msgStatus.setAttribute("class","valida");
+          msgStatus.innerText = "";
+          window.location.href = "../erro.html";
+      }, 3000);
       return false;
   }
   
