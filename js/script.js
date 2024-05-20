e83981ce7fb4309800fa84a474ee7c408df8fb62
dudabrigidio <dudabrigidio@gmail.com>
2024-05-20 11:37:50 -0300

    Estilização CSS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // console.log("Olá Mundo!");
 
 // console.log(document.getElementById("botao").innerHTML);
@@ -13,7 +12,7 @@
 //Hoisting
 // var nome = "Cleiton";
 //  console.log(nome);
- 
+
 //  if(true){
 //     let nome = "José";
 //  }
@@ -99,7 +98,7 @@
 //   console.log(`Indice = ${i} - Valor = ${arr1[i]}` );
 // }
 
-//Imprindo o array com forOf 
+//Imprindo o array com forOf
 // for (const valor of arr1) {
 //   console.log(valor);
 // }
@@ -139,55 +138,145 @@
 //INICIAR UM SISTEMA DE VALIDAÇÃO
 
 //Recuperando o botão de submit do Formulário
-const botaoSubmit = document.querySelector("#btnSubmit");
+// const botaoSubmit = document.querySelector("#btnSubmit");
 
-//Adicionar um evento ao botão quando ocorrer.
-//botaoSubmit.addEventListener("click", function(e){
-    //Recuperando os campos do formulário para validação!
-   // e.preventDefault();  //Evita que o formulário seja enviado
-   // const inputEmail = document.querySelector("#idEmail");
-   // const inputSenha = document.querySelector("#idSenha");
-   // console.log(inputEmail.value)
-   // console.log(inputSenha.value)
-//}
+// //Adicionar um evento ao botão quando ocorrer.
+// botaoSubmit.addEventListener("click", function(e){
+//     //Recuperando os campos do formulário para validação!
+//     e.preventDefault();  //Evita que o formulário seja enviado
+//     const inputEmail = document.querySelector("#idEmail");
+//     const inputSenha = document.querySelector("#idSenha");
+//     console.log(inputEmail.value)
+//     console.log(inputSenha.value)
+// })
 
+// let usuario = {
+//     nomeCompleto: "Jose das Couves",
+//     emailUsuario: "jo@email.com",
+//     senhaUsuario: "123456"
+// }
 
-function validar(){
-
-    //if((a+b)> 10) {
-    //    alert('Você clicou no botão tradição');
-    //    return true;
-    //} else {
-    //    alert('Inválido');
-    //    return false;
-    //}
-
-    // Pegando o elemento input
-    
-    //const inputLabel = document.querySelector("label[for-idEmails")
-
-    //Adicionar dados no innerText
-    // // inputLabel.innerText = '<p>Digite seu Email</p>';
-    // inputLabel.innerHTML = '<p>Digite seu Email</p>';
-
-    // console.log("INNER HTML : " + inputLabel.innerHTML);
-    // console.log("INNER TEXT : " + inputLabel.innerText);
-
-
-    const inputEmail = document.getElementById("idEmail").value;
-    console.log(inputEmail);
-
-    const inputSenha = document.getElementById("idSenha").value;
-    console.log(inputSenha);
-
-    if(inputEmail == "dudabrigidio@gmail.com" && inputSenha == "123456"){
-        //Redirect 
-        window.location.href = "../sucesso.html";
-    } else {
-        //Redirect 
-        window.location.href = "../erro.html";
-    }
-
-
-    return false;
-}
+let listaUsuario = [
+    {
+      nomeCompleto: "Jose da Silva",
+      emailUsuario: "jo@email.com",
+      senhaUsuario: "123456",
+    },
+    {
+      nomeCompleto: "Joao Antonio",
+      emailUsuario: "an@email.com",
+      senhaUsuario: "123456",
+    },
+    {
+      nomeCompleto: "Marco Antonio",
+      emailUsuario: "ma@email.com",
+      senhaUsuario: "123456",
+    },
+    {
+      nomeCompleto: "Carlos Silva",
+      emailUsuario: "ca@email.com",
+      senhaUsuario: "123456",
+    },
+    {
+      nomeCompleto: "Luis Claudio",
+      emailUsuario: "lc@email.com",
+      senhaUsuario: "123456",
+    },
+  ];
+  
+  function validar(inputEmail, inputSenha) {
+  
+  //Recuperar elemento de msgStatus
+  let msgStatus = document.querySelector(".valida");
+  
+  for (let x = 0; x < listaUsuario.length; x++) {
+      
+      if ((inputEmail.value == listaUsuario[x].emailUsuario) && (inputSenha.value == listaUsuario[x].senhaUsuario)) {
+        //Redirect
+          msgStatus.setAttribute("class","sucesso");
+          msgStatus.innerText = "Login realizado com sucesso!";
+  
+          //Guardando o objeto validado no localStorage:
+          localStorage.setItem("usuario-logado", JSON.stringify(listaUsuario[x]));
+          
+          setTimeout(function(){
+              msgStatus.setAttribute("class","valida");
+              msgStatus.innerText = "";
+              window.location.href = "../sucesso.html";
+          }, 3000);
+          return false;
+      }
+  }
+      msgStatus.setAttribute("class","erro");
+      msgStatus.innerText = "Login ou senha invalidos!";
+      setTimeout(function(){
+          msgStatus.setAttribute("class","valida");
+          msgStatus.innerText = "";
+          window.location.href = "../erro.html";
+      }, 3000);
+      return false;
+  }
+  
+  let inputCpf = document.querySelector("#idCpf");
+  inputCpf.addEventListener("input", ()=>{
+      let cpf = inputCpf.value;
+  
+      //Removendo os caractéres indesejados com regex:
+      cpf = cpf.replace(/\D/g, "");
+      //Aplicando a mascara ao CPF e adicionando pontos e traço nos lugares corretos.
+      cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+      cpf = cpf.replace(/(\d{3})(\d)/, "$1.$2");
+      cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+  
+      inputCpf.value = cpf;
+  });
+  
+  //EXERCÍCIOS
+  
+  // Exercício 1 - Variáveis e Hoisting:
+  // Qual será o valor de nome após a execução do código abaixo?
+  // "use strict";
+  // var nome = "Joaquim";
+  // if (true) {
+  //     let nome = "João";
+  // }
+  // console.log(nome);
+  
+  // Exercício 2 - Declaração de Variáveis:
+  // Declare uma variável estrito utilizando let sem atribuir um valor. Imprima o valor de estrito no console.
+  
+  // Exercício 3 - Tipos de Dados:
+  // Qual é o tipo de dado da variável str?
+  // let str = "Olá, mundo!";
+  
+  // Exercício 4 - Mesclar Arrays:
+  // Crie um novo array chamado arrConcatenado que seja a concatenação dos arrays arr1 e arr2 dos exemplos passados. Imprima arrConcatenado no console.
+  
+  // Exercício 5 - Acesso a Propriedades de Objetos:
+  // Acesse a propriedade idade do objeto obj e imprima seu valor no console.
+  // let obj = { nome: "João", idade: 25, devedor: true };
+  
+  // Exercício 6 - Atributo Variável de Objetos:
+  // Atribua a string "São Paulo" à propriedade cidade do objeto obj da questão 5. Imprima o obj no console.
+  
+  // Exercício 7 - Conversão de String para Número:
+  // Converta a string "10" em um número e imprima o resultado no console.
+  
+  // Exercício 8 - Estrutura Condicional com Operadores:
+  // Qual será a saída do código abaixo?
+  // let idade = 20;
+  // let result = (idade >= 18) ? "Maior de idade" : "Menor de idade";
+  // console.log(result);
+  
+  // Exercício 9 - Estrutura Condicional Tradicional:
+  // Qual será a saída do código abaixo?
+  // let age = 20;
+  // if (age >= 18) {
+  //     console.log("Maior de idade");
+  // } else {
+  //     console.log("Menor de idade");
+  // }
+  
+  // Exercício 10 - Mesclar Arrays com Spread:
+  // Crie um novo array chamado arr4 que seja a concatenação dos arrays arr1 e arr2 dos exemplospassados e utilizando o operador spread. Imprima arr4 no console.
+  
