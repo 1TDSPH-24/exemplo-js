6cab781772663edb1703aa53d20610a755cec901
cotete <feliperibeirots@gmail.com>
2024-05-25 12:44:30 -0300

    removendo comentarios

--- js/script.js
+++ js/script.js
@@ -74,54 +74,6 @@ inputCpf.addEventListener("input", ()=>{
     inputCpf.value = cpf;
 });
 
-//EXERCÍCIOS
-
-// Exercício 1 - Variáveis e Hoisting:
-// Qual será o valor de nome após a execução do código abaixo?
-// "use strict";
-// var nome = "Joaquim";
-// if (true) {
-//     let nome = "João";
-// }
-// console.log(nome);
-
-// Exercício 2 - Declaração de Variáveis:
-// Declare uma variável estrito utilizando let sem atribuir um valor. Imprima o valor de estrito no console.
-
-// Exercício 3 - Tipos de Dados:
-// Qual é o tipo de dado da variável str?
-// let str = "Olá, mundo!";
-
-// Exercício 4 - Mesclar Arrays:
-// Crie um novo array chamado arrConcatenado que seja a concatenação dos arrays arr1 e arr2 dos exemplos passados. Imprima arrConcatenado no console.
-
-// Exercício 5 - Acesso a Propriedades de Objetos:
-// Acesse a propriedade idade do objeto obj e imprima seu valor no console.
-// let obj = { nome: "João", idade: 25, devedor: true };
-
-// Exercício 6 - Atributo Variável de Objetos:
-// Atribua a string "São Paulo" à propriedade cidade do objeto obj da questão 5. Imprima o obj no console.
-
-// Exercício 7 - Conversão de String para Número:
-// Converta a string "10" em um número e imprima o resultado no console.
-
-// Exercício 8 - Estrutura Condicional com Operadores:
-// Qual será a saída do código abaixo?
-// let idade = 20;
-// let result = (idade >= 18) ? "Maior de idade" : "Menor de idade";
-// console.log(result);
-
-// Exercício 9 - Estrutura Condicional Tradicional:
-// Qual será a saída do código abaixo?
-// let age = 20;
-// if (age >= 18) {
-//     console.log("Maior de idade");
-// } else {
-//     console.log("Menor de idade");
-// }
-
-// Exercício 10 - Mesclar Arrays com Spread:
-// Crie um novo array chamado arr4 que seja a concatenação dos arrays arr1 e arr2 dos exemplospassados e utilizando o operador spread. Imprima arr4 no console.
 
 //Recuperando o botao login:
 const botaoLogin = document.querySelector("#btnLogin");
